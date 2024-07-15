8b7878ef75189d582201b4d998b4bdf53958d441
G-Akleh <ghadeer_akleh@hotmail.com>
2024-07-15 16:21:48 +0300

    Set the bot commands list

--- bot/setup.py
+++ bot/setup.py
@@ -1,8 +1,9 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram import BotCommand
 from handlers.message_handlers import echo, update_command, update_with_file
 from handlers.command_handlers import start, help_command
 from config import TELEGRAM_BOT_TOKEN
-
+import asyncio
 
 def setup_telegram_bot():
     """
@@ -24,4 +25,23 @@ def setup_telegram_bot():
     bot.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     bot.add_handler(MessageHandler(filters.Document.ALL, update_with_file))
 
+    # Set the bot commands
+    asyncio.run(set_commands(bot.bot))
+
     return bot
+
+
+async def set_commands(bot):
+    """
+    Sets the commands list of the bot and to show them when typing a slash "/"
+
+    Args:
+        bot: an object of type telegram.bot
+    """
+    commands = [
+        BotCommand("start", "Start the bot"),
+        BotCommand("help", "Show help information"),
+        BotCommand("upd", "Update the bot"),
+        BotCommand("ask", "Ask the bot a question"),
+    ]
+    await bot.set_my_commands(commands)
