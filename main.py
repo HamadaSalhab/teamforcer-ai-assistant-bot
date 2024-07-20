c3e604f314cb2d290e31f239a73723e5fb148630
bruteforceboy <ogbonnachibuoyim12@gmail.com>
2024-07-20 16:40:10 +0300

    add database and update chat context

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 import nest_asyncio
 from bot.setup import setup_telegram_bot
+from threading import Thread
+from app import app
 
 # Apply nest_asyncio to allow nested asynchronous event loops
 nest_asyncio.apply()
 
-# Setup and initialize the Telegram bot
-bot = setup_telegram_bot()
+def run_flask():
+    app.run(host='0.0.0.0', port=5000)
 
-# Start polling for updates from Telegram
-bot.run_polling(close_loop=False)
+def main():
+    # Setup and initialize the Telegram bot
+    bot = setup_telegram_bot()
+
+    # Start the Flask app in a separate thread
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
+
+    # Start polling for updates from Telegram
+    bot.run_polling(close_loop=False)
+
+if __name__ == "__main__":
+    main()
