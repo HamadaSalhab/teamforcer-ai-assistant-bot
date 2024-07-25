a68f8f2885b3799f843cc49784351ec08d1ec207
HamadaSalhab <hamada.a.salhab@gmail.com>
2024-07-25 14:01:57 +0300

    Shrink messages upon exceeding token limit

--- model/chat_model.py
+++ model/chat_model.py
@@ -5,6 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
 from config import MODEL_NAME, OPENAI_API_KEY
 from storage.sqlalchemy_database import get_chat_history
+from .utils import exceeds_model_tokens_limit
 from langchain_core.messages.base import BaseMessage
 
 
@@ -57,6 +58,15 @@ def get_answer(query: str, chat: ChatOpenAI, vectorstore: PineconeVectorStore, m
     augmented_prompt = augment_prompt(query, vectorstore)
 
     messages.append(HumanMessage(content=augmented_prompt))
+
+    while exceeds_model_tokens_limit("".join(message.content for message in messages)):
+        # Remove the oldest human-AI message pair
+        if len(messages) > 4:  # Keep the initial SystemMessage and at least one exchange
+            messages.pop(3)
+            messages.pop(3)
+        else:
+            # Only occurs when the first human-AI message pair + first augmented prompt exceed token limit
+            return "Произошла ошибка.\n(Код ошибки: 0x310).\n\nПожалуйста, сообщите об этом разработчику бота @hamadasalhab"
 
     # Generate response using the chat model
     res = chat.invoke(messages)
